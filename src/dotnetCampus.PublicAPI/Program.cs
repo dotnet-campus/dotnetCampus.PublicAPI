6e86fb2fc3275a1bef1c9d24d9e978df78c42cfd
walterlv <xblero@gmail.com>
2019-08-01 11:30:21 +0800

    提前准备好文件

--- src/dotnetCampus.PublicAPI/Program.cs
+++ src/dotnetCampus.PublicAPI/Program.cs
@@ -11,7 +11,12 @@ namespace dotnetCampus.PublicAPI
                 return;
             }
 
-            if (args[0] is "generate")
+            if (args[0] is "prepare")
+            {
+                // 确保 API 文件存在。
+                new PrepareApiFileTask().Execute(args);
+            }
+            else if (args[0] is "generate")
             {
                 // 组织目标项目的文件夹结构。
                 new GenerateTask().Execute(args);
---
+++ src/dotnetCampus.PublicAPI/Tasks/PrepareApiFileTask.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dotnetCampus.PublicAPI.Core;
+
+namespace dotnetCampus.PublicAPI.Tasks
+{
+    internal class PrepareApiFileTask : IPackageTask
+    {
+        public void Execute(string[] args)
+        {
+            PrepareApiFiles(args[2], args[4]);
+        }
+
+        private void PrepareApiFiles(string apiFile, string shippedApiFile)
+        {
+            var directory = Path.GetDirectoryName(apiFile);
+            if (!Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory);
+            }
+
+            if (!File.Exists(apiFile))
+            {
+                File.WriteAllText(apiFile, "");
+            }
+            if (!File.Exists(shippedApiFile))
+            {
+                File.WriteAllText(shippedApiFile, "");
+            }
+        }
+    }
+}
